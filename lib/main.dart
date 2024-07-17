ac8ff03598798c635b60964c742dd608044057d8
olleh365 <olleh365@gmail.com>
2024-07-17 22:35:09 +0900

    새로고침 버튼 수정

--- lib/main.dart
+++ lib/main.dart
@@ -26,31 +26,54 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('소프트런치'),
-          backgroundColor: Colors.white,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Container(
-                width: 85,
-                height: 32,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(4),
-                ),
-                child: TextButton(
-                  onPressed: toastExample,
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.fromLTRB(10, 6, 8, 6),
-                    textStyle: const TextStyle(fontSize: 14),
-                    foregroundColor: Colors.white,
+          title: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 0.0),
+            child: SizedBox(
+              width: 100,
+              height: 24,
+              // alignment: Alignment.center,\
+              child: Row(
+                children: [
+                  Text(
+                    '소프트런치',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
                   ),
-                  child: const Text('새로고침'),
-                ),
+                  SizedBox(width: 4),
+                  SizedBox(
+                    width: 24,
+                    height: 24,
+                    child: Placeholder(),
+                  ),
+                  // SizedBox(width: 4)
+                ],
               ),
-            )
-          ],
+          ),
         ),
+        backgroundColor: Colors.white,
+        //새로 고침 버튼
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Container(
+              width: 85,
+              height: 32,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: TextButton(
+                onPressed: toastExample,
+                style: TextButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 14),
+                  foregroundColor: Colors.white,
+
+                ),
+                child: const Text('새로고침'),
+              ),
+            ),
+          ),
+        ],
+      ),
       ),
     );
   }
