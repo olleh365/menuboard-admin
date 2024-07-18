78714e250ba1fadbfef276ef3b79c870f6a700af
junwoo.h <junwoo.h@softlunch.com>
2024-07-18 18:10:35 +0900

    헤더에 Tabbar 구현

--- lib/main.dart
+++ lib/main.dart
@@ -24,57 +24,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 0.0),
-            child: SizedBox(
-              width: 100,
-              height: 24,
-              // alignment: Alignment.center,\
-              child: Row(
-                children: [
-                  Text(
-                    '소프트런치',
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-                  ),
-                  SizedBox(width: 4),
-                  SizedBox(
-                    width: 24,
-                    height: 24,
-                    child: Placeholder(),
-                  ),
-                  // SizedBox(width: 4)
-                ],
-              ),
-          ),
-        ),
-        backgroundColor: Colors.white,
-        //새로 고침 버튼
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18.0),
-            child: Container(
-              width: 85,
-              height: 32,
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(4),
-              ),
-              child: TextButton(
-                onPressed: toastExample,
-                style: TextButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 14),
-                  foregroundColor: Colors.white,
-
+      home: DefaultTabController(
+        length: 4,
+        child : Scaffold(
+          appBar: AppBar(
+            title: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 0.0),
+              child: SizedBox(
+                width: 100,
+                height: 24,
+                // alignment: Alignment.center,\
+                child: Row(
+                  children: [
+                    Text(
+                      '소프트런치',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(width: 4),
+                    // 로그인 소셜 계정 아이콘
+                    SizedBox(
+                      width: 24,
+                      height: 24,
+                      child: Placeholder(),
+                    ),
+                  ],
                 ),
-                child: const Text('새로고침'),
-              ),
             ),
           ),
-        ],
+          backgroundColor: Colors.white,
+          bottom:  const TabBar(tabs: [Tab(text: '대기'),Tab(text: '주방'),Tab(text: '서빙'),Tab(text: '현황')],
+          indicatorColor: Color(0xFFFF662B),
+          indicatorSize: TabBarIndicatorSize.tab,
+          labelColor: Color(0xFFFF662B),
+          unselectedLabelColor: Color(0xFF989898),
+          splashFactory: NoSplash.splashFactory),
+          //새로 고침 버튼
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: Container(
+                width: 85,
+                height: 32,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(4),
+                ),
+                child: TextButton(
+                  onPressed: toastExample,
+                  style: TextButton.styleFrom(
+                    textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+                    foregroundColor: Colors.white,
+                  ),
+                  child: const Text('새로고침'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      ),
+      )
     );
   }
 }
