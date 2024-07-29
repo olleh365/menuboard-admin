ac1e5b1250fe2b35b56492f2c142eea09f90207a
junwoo.h <junwoo.h@softlunch.com>
2024-07-29 21:54:42 +0900

    현황탭 팝업창 UI구현

--- lib/status_screen.dart
+++ lib/status_screen.dart
@@ -13,7 +13,7 @@ class StatusScreen extends StatefulWidget {
 }
 
 class StatusScreenState extends State<StatusScreen>{
-var f = NumberFormat('###,###,###,###');
+  var f = NumberFormat('###,###,###,###');
 
   @override
   Widget build(BuildContext context) {
@@ -42,113 +42,113 @@ var f = NumberFormat('###,###,###,###');
         return GestureDetector(
           onTap: () => tableDialog(context, tableNumber, order),
           child: Column(
-          children: [
-            Container(
-              height: 152,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8)
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 8),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              '테이블 $tableNumber',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w700,
-                                color: order.items.isEmpty ? const Color(0xFF777777): const Color(0xFFFF662B),
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                            width: 71,
-                            height: 24,
-                            child: TextButton(
-                                onPressed: toastExample,
-                                style: TextButton.styleFrom(
-                                  backgroundColor: const Color(0xFFFF662B),
-                                  foregroundColor: Colors.white,
-                                  padding: EdgeInsets.zero,
-                                ),
-                                child: const Text(
-                                  '테이블 리셋',
-                                  style: TextStyle(
-                                      fontSize: 12, fontWeight: FontWeight.w600
-                                  ),
-                                )
-                            )
-                        )
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                    ...order.items.take(3).map((item) =>
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  item.mainMenu,
-                                  style: const TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w600,
-                                      color: Colors.black
-                                  ),
-                                ),
-                              ],
-                            ),
-                            // 개수
-                            Text(
-                                '${item.quantity}개',
-                              style: const TextStyle(color: Color(0xFFAAAAAA),fontWeight: FontWeight.w500,fontSize: 12),
-                            ),
-                          ],
-                        ),
-                      )),
-                  const Spacer(),
-                  const Divider(
-                    color: Color(0xFFF5F5F5),
-                    height: 1,
-                    thickness: 1,
-                  ),
-                  Container(
-                    height: 38,
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
-                    alignment: Alignment.centerLeft,
-                    child: Row(
+            children: [
+              Container(
+                height: 152,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text('외 $otherQuantity항목',style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500,color: Color(0xFF777777)),),
-                          Text(
-                            '${f.format(totalPrice)}원',
-                            style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w700,
-                              color: Colors.black,
-                            ),
+                          Row(
+                            children: [
+                              Text(
+                                '테이블 $tableNumber',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w700,
+                                  color: order.items.isEmpty ? const Color(0xFF777777): const Color(0xFFFF662B),
+                                ),
+                              ),
+                            ],
                           ),
-                      ]
+                          SizedBox(
+                              width: 71,
+                              height: 24,
+                              child: TextButton(
+                                  onPressed: toastExample,
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: const Color(0xFFFF662B),
+                                    foregroundColor: Colors.white,
+                                    padding: EdgeInsets.zero,
+                                  ),
+                                  child: const Text(
+                                    '테이블 리셋',
+                                    style: TextStyle(
+                                        fontSize: 12, fontWeight: FontWeight.w600
+                                    ),
+                                  )
+                              )
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 8),
+                    ...order.items.take(3).map((item) =>
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item.mainMenu,
+                                    style: const TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              // 개수
+                              Text(
+                                '${item.quantity}개',
+                                style: const TextStyle(color: Color(0xFFAAAAAA),fontWeight: FontWeight.w500,fontSize: 12),
+                              ),
+                            ],
+                          ),
+                        )),
+                    const Spacer(),
+                    const Divider(
+                      color: Color(0xFFF5F5F5),
+                      height: 1,
+                      thickness: 1,
+                    ),
+                    Container(
+                      height: 38,
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      alignment: Alignment.centerLeft,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('외 $otherQuantity항목',style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500,color: Color(0xFF777777)),),
+                            Text(
+                              '${f.format(totalPrice)}원',
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ]
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
         );
       },
     );
@@ -158,62 +158,169 @@ var f = NumberFormat('###,###,###,###');
     showDialog(
         context: context,
         builder: (context) {
+          final formattedTime = DateFormat('HH:mm').format(order.orderTime);
           return AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8),
-            ),
+            backgroundColor: const Color(0xFFF5F5F5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
               contentPadding: EdgeInsets.zero,
-              content: SizedBox(
+              // 기본 구성 요소들이 shape 속성을 덮어씌우기 때문에 ClipRRect 위젯 사용
+              content: ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child : SizedBox(
                 height: 800,
                 width: 600,
                 child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                Padding(
-                padding: const EdgeInsets.only(left: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      '테이블 $tableNumber',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700,
-                        color: order.items.isEmpty ? const Color(0xFF777777): const Color(0xFFFF662B),),
+                    Container(
+                      padding: const EdgeInsets.only(left: 16),
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '테이블 $tableNumber',
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700,
+                              color: order.items.isEmpty ? const Color(0xFF777777): const Color(0xFFFF662B),),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.close),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      ),
                     ),
-                    IconButton(
-                      icon: const Icon(Icons.close),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
+                    const Divider(
+                      color: Color(0xFFF5F5F5),
+                      height: 1,
+                      thickness: 1,
                     ),
+                    Container(
+                      color: Colors.white,
+                      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '현재 주문중',
+                            style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: Color(0xFFFF662B)),
+                          ),
+                          Row(
+                              children:[
+                                Text(
+                                  '주문번호 ${order.orderNumber}',
+                                  style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                                ),
+                                Text(' ($formattedTime)',
+                                  style: const TextStyle(fontSize: 13,fontWeight: FontWeight.w500,color: Color(0xFF777777),),
+                                ),
+                              ],
+                          ),
+                        const SizedBox(height: 4,),
+                        ...order.items.map((item) =>
+                            Column(
+                              children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                                width: 400,
+                                  decoration: BoxDecoration(
+                                    color: const Color(0xFFF5F5F5),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children:[
+                                    Text(
+                                      '${item.mainMenu}, ${item.quantity}개',
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700,
+                                          color: Colors.black
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          '${item.additionalMenu.map((
+                                              addItem) => addItem.name).join(' / ')} 추가',
+                                          style: const TextStyle(
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.w500,
+                                            color: Color(0xFF777777),
+                                          ),
+                                        ),
+                                        Text(
+                                          '${f.format(item.totalPrice)}원',
+                                          style: const TextStyle(color: Color(0xFF777777),fontSize: 14, fontWeight: FontWeight.w600),
+                                        )
+                                      ],
+                                    ),
+                              ]
+                            ),
+
+                            ),
+                                const SizedBox(height: 8,)
+                          ],
+                          ),
+                        ),
+                          const SizedBox(height: 8),
+                          CustomPaint(
+                            painter: DotDivider(),
+                            size: const Size(double.infinity,3),
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('총 금액',
+                                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),),
+                                Text('${f.format(order.totalPrice)}원',
+                                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w700),)
+                              ],
+                            ),
+                          )
+
+                        ],
+                      ),
+                    )
                   ],
                 ),
+              )
               ),
-              const Divider( // 구분선 추가
-                color: Color(0xFFF5F5F5),
-                height: 1,
-                thickness: 1,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ListView(
-                    children: order.items
-                        .map((item) => Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 4),
-                      child: Text(
-                        '${item.mainMenu}, ${item.quantity}개',
-                        style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-                      ),
-                    ))
-                        .toList(),
-                  ),
-                ),
-              ),
-            ],
-            ),
-            )
           );
         }
     );
+  }
+}
+
+// 점선 구분선 생성
+class DotDivider extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = const Color(0xFF777777)
+      ..strokeWidth = 1
+      ..style = PaintingStyle.stroke;
+
+    var max = size.width;
+    var dashWidth = 5;
+    var dashSpace = 5;
+    double startX = 0;
+
+    while (startX < max) {
+      canvas.drawLine(Offset(startX, 0), Offset(startX + dashWidth, 0), paint);
+      startX += dashWidth + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
