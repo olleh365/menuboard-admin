13f32e33a982925981d353c3bf9b8323ec3b7b93
junwoo.h <junwoo.h@softlunch.com>
2024-08-01 18:37:41 +0900

    주문 모델 파일 생성 및 DIO, Freezed, json_serializable 등 라이브러리 추가

---
+++ lib/order_model.dart
@@ -0,0 +1,71 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'order_model.freezed.dart';
+part 'order_model.g.dart';
+
+@freezed
+class OrderResponse with _$OrderResponse {
+  factory OrderResponse({
+    required List<OrderGroup> data,
+  }) = _OrderResponse;
+
+  factory OrderResponse.fromJson(Map<String, dynamic> json) => _$OrderResponseFromJson(json);
+}
+
+@freezed
+class OrderGroup with _$OrderGroup {
+  factory OrderGroup({
+    required int orderGroupNum,
+    required int tableNum,
+    required int totalOrderPrice,
+    required List<Order> orders,
+  }) = _OrderGroup;
+
+  factory OrderGroup.fromJson(Map<String, dynamic> json) => _$OrderGroupFromJson(json);
+}
+
+@freezed
+class Order with _$Order {
+  factory Order({
+    required int orderSeq,
+    required int memberSeq,
+    required int storeSeq,
+    required String orderNum,
+    required String orderStatus,
+    required int tableNum,
+    required int orderTotalPrice,
+    required int orderTotalOriginalPrice,
+    required DateTime orderDate,
+    required DateTime orderCompleteDate,
+    required int orderGroupNum,
+    required List<Menu> menuList,
+  }) = _Order;
+
+  factory Order.fromJson(Map<String, dynamic> json) => _$OrderFromJson(json);
+}
+
+@freezed
+class Menu with _$Menu {
+  factory Menu({
+    required int orderMenuSeq,
+    required String orderMenuDelYn,
+    required int menuSeq,
+    required String menuName,
+    required int quantity,
+    required int menuTotalPrice,
+    required int menuTotalOriginalPrice,
+    required List<Option> selectedOptions,
+  }) = _Menu;
+
+  factory Menu.fromJson(Map<String, dynamic> json) => _$MenuFromJson(json);
+}
+
+@freezed
+class Option with _$Option {
+  factory Option({
+    required int menuOptionSeq,
+    required String menuOptionName,
+}) = _Option;
+
+  factory Option.fromJson(Map<String, dynamic> json) => _$OptionFromJson(json);
+}
---
+++ lib/order_model.g.dart
@@ -0,0 +1,106 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'order_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$OrderResponseImpl _$$OrderResponseImplFromJson(Map<String, dynamic> json) =>
+    _$OrderResponseImpl(
+      data: (json['data'] as List<dynamic>)
+          .map((e) => OrderGroup.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$OrderResponseImplToJson(_$OrderResponseImpl instance) =>
+    <String, dynamic>{
+      'data': instance.data,
+    };
+
+_$OrderGroupImpl _$$OrderGroupImplFromJson(Map<String, dynamic> json) =>
+    _$OrderGroupImpl(
+      orderGroupNum: (json['orderGroupNum'] as num).toInt(),
+      tableNum: (json['tableNum'] as num).toInt(),
+      totalOrderPrice: (json['totalOrderPrice'] as num).toInt(),
+      orders: (json['orders'] as List<dynamic>)
+          .map((e) => Order.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$OrderGroupImplToJson(_$OrderGroupImpl instance) =>
+    <String, dynamic>{
+      'orderGroupNum': instance.orderGroupNum,
+      'tableNum': instance.tableNum,
+      'totalOrderPrice': instance.totalOrderPrice,
+      'orders': instance.orders,
+    };
+
+_$OrderImpl _$$OrderImplFromJson(Map<String, dynamic> json) => _$OrderImpl(
+      orderSeq: (json['orderSeq'] as num).toInt(),
+      memberSeq: (json['memberSeq'] as num).toInt(),
+      storeSeq: (json['storeSeq'] as num).toInt(),
+      orderNum: json['orderNum'] as String,
+      orderStatus: json['orderStatus'] as String,
+      tableNum: (json['tableNum'] as num).toInt(),
+      orderTotalPrice: (json['orderTotalPrice'] as num).toInt(),
+      orderTotalOriginalPrice: (json['orderTotalOriginalPrice'] as num).toInt(),
+      orderDate: DateTime.parse(json['orderDate'] as String),
+      orderCompleteDate: DateTime.parse(json['orderCompleteDate'] as String),
+      orderGroupNum: (json['orderGroupNum'] as num).toInt(),
+      menuList: (json['menuList'] as List<dynamic>)
+          .map((e) => Menu.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$OrderImplToJson(_$OrderImpl instance) =>
+    <String, dynamic>{
+      'orderSeq': instance.orderSeq,
+      'memberSeq': instance.memberSeq,
+      'storeSeq': instance.storeSeq,
+      'orderNum': instance.orderNum,
+      'orderStatus': instance.orderStatus,
+      'tableNum': instance.tableNum,
+      'orderTotalPrice': instance.orderTotalPrice,
+      'orderTotalOriginalPrice': instance.orderTotalOriginalPrice,
+      'orderDate': instance.orderDate.toIso8601String(),
+      'orderCompleteDate': instance.orderCompleteDate.toIso8601String(),
+      'orderGroupNum': instance.orderGroupNum,
+      'menuList': instance.menuList,
+    };
+
+_$MenuImpl _$$MenuImplFromJson(Map<String, dynamic> json) => _$MenuImpl(
+      orderMenuSeq: (json['orderMenuSeq'] as num).toInt(),
+      orderMenuDelYn: json['orderMenuDelYn'] as String,
+      menuSeq: (json['menuSeq'] as num).toInt(),
+      menuName: json['menuName'] as String,
+      quantity: (json['quantity'] as num).toInt(),
+      menuTotalPrice: (json['menuTotalPrice'] as num).toInt(),
+      menuTotalOriginalPrice: (json['menuTotalOriginalPrice'] as num).toInt(),
+      selectedOptions: (json['selectedOptions'] as List<dynamic>)
+          .map((e) => Option.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$MenuImplToJson(_$MenuImpl instance) =>
+    <String, dynamic>{
+      'orderMenuSeq': instance.orderMenuSeq,
+      'orderMenuDelYn': instance.orderMenuDelYn,
+      'menuSeq': instance.menuSeq,
+      'menuName': instance.menuName,
+      'quantity': instance.quantity,
+      'menuTotalPrice': instance.menuTotalPrice,
+      'menuTotalOriginalPrice': instance.menuTotalOriginalPrice,
+      'selectedOptions': instance.selectedOptions,
+    };
+
+_$OptionImpl _$$OptionImplFromJson(Map<String, dynamic> json) => _$OptionImpl(
+      menuOptionSeq: (json['menuOptionSeq'] as num).toInt(),
+      menuOptionName: json['menuOptionName'] as String,
+    );
+
+Map<String, dynamic> _$$OptionImplToJson(_$OptionImpl instance) =>
+    <String, dynamic>{
+      'menuOptionSeq': instance.menuOptionSeq,
+      'menuOptionName': instance.menuOptionName,
+    };
