9b4e54dada2b5449f2d27965e3b871e7782ec45d
junwoo.h <junwoo.h@softlunch.com>
2024-08-08 18:02:33 +0900

    웹 연동 기초 및 Pub 라이브러리 업그레이드

--- lib/waiting_screen.dart
+++ lib/waiting_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
@@ -159,6 +160,7 @@ class WaitingScreenState extends State<WaitingScreen> with AutomaticKeepAliveCli
         final order = _orders[index];
         final formattedTime = DateFormat('HH:mm').format(order.orderDate);
         return Column(
+          verticalDirection: VerticalDirection.down,
           children: [
             const SizedBox(height: 8),
             Container(
